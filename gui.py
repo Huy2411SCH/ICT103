86cd6ed9f6c9cd2cdd871709be0553ca5528349f
Huy2411SCH <huyle2411sch@gmail.com>
2024-10-04 21:31:13 +1000

    A basic login system connect to a database

--- gui.py
+++ gui.py
@@ -1,92 +1,105 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
-
-window = tk.Tk()
-window.title("Hello World")
-#window.iconbitmap('./assets/pythontutorial.ico')
-window_width = 500
-window_height = 300
-# the ability to resize
-window.resizable(True, True)
-window.minsize(500, 300)
-window.maxsize(1920, 1080)
-# get the screen dimension
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
-
-# find the center point
-center_x = int(screen_width/2 - window_width / 2)
-center_y = int(screen_height/2 - window_height / 2)
-
-# set the position of the window to the center of the screen
-window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
-
-
-# Sign in frame
-signin = ttk.Frame(window)
-signin.pack(padx=10, pady=10, fill='x', expand=True)
-
-# store email address and password
-email = tk.StringVar()
-password = tk.StringVar()
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+import sqlite3
+import pandas as pd
 
 class LoginSystem:
-    def __init__(self):
-        self.users = []
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+        self.create_user_table()
+    def create_user_table(self):
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+                username TEXT PRIMARY KEY,
+                password TEXT NOT NULL
+        )
+        ''')
+        self.conn.commit()
+   
 
-    def register(self, username, password):
-        user = User(username, password)
-        self.users.append(user)
-        print(f"User {username} registered successfully!")
-
+    def add_user(self, username, password):
+        try:
+            self.cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+            self.conn.commit()
+            return "User added successfully!"
+        except sqlite3.IntegrityError:
+            return "Username already exists!"
     def login(self, username, password):
-        for user in self.users:
-            if user.username == username and user.password == password:
-                print(f"Welcome, {username}!")
-                return True
-        print("Invalid username or password.")
-        return False
+        
+            self.cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?',(username, password ))
+            self.result= self.cursor.fetchone()
 
-def login_clicked():
-    """ callback when the login button clicked
-    """
-    msg = f'You entered email: {email.get()} and password: {password.get()}'
-    showinfo(
-        title='Information',
-        message=msg
-    )
-LoginSys = LoginSystem()
-def register_clicked():
-    LoginSys.register(email.get(),password.get())
+            return self.result
 
-# email
-email_label = ttk.Label(signin, text="Email Address:")
-email_label.pack(fill='x',padx=100, pady=0, expand=True)
 
-email_entry = ttk.Entry(signin, textvariable=email)
-email_entry.pack(fill='x',padx=100, pady=0, expand=True)
 
-email_entry.focus()
+class LoginWin:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Login UI")
+        #window.iconbitmap('./assets/pythontutorial.ico')
+        self.window_width = 500
+        self.window_height = 300 
+        self.root.resizable = (True, True)
+        self.root.minsize(500,300)
+        self.root.maxsize(1920,1080)
+        self.screen_width = root.winfo_screenwidth()
+        self.screen_height = root.winfo_screenheight()
+        self.center_x = int(self.screen_width/2 - self.window_width / 2)
+        self.center_y = int(self.screen_height/2 - self.window_height / 2)
+        self.root.geometry(f'{self.window_width}x{self.window_height}+{self.center_x}+{self.center_y}')
+        self.email = tk.StringVar()
+        self.password = tk.StringVar()
+        
+        # Sign in frame
+        self.signin = ttk.Frame(self.root)
+        self.signin.pack(padx=10, pady=10, fill='x', expand=True)
+        # email
+        self.email_label = ttk.Label(self.signin, text="Email Address:")
+        self.email_label.pack(fill='x',padx=100, pady=0, expand=True)
+
+        self.email_entry = ttk.Entry(self.signin, textvariable=self.email)
+        self.email_entry.pack(fill='x',padx=100, pady=0, expand=True)
+
+        self.email_entry.focus()
 
 # password
-password_label = ttk.Label(signin, text="Password:")
-password_label.pack(fill='x',padx=100, pady=0, expand=True)
+        self.password_label = ttk.Label(self.signin, text="Password:")
+        self.password_label.pack(fill='x',padx=100, pady=0, expand=True)
 
-password_entry = ttk.Entry(signin, textvariable=password, show="*")
-password_entry.pack(fill='x',padx=100, pady=0, expand=True)
+        self.password_entry = ttk.Entry(self.signin, textvariable=self.password, show="*")
+        self.password_entry.pack(fill='x',padx=100, pady=0, expand=True)
 
-# login button
-login_button = ttk.Button(signin, text="Login", command=login_clicked)
-login_button.pack(fill='x',padx=100, pady=10, expand=True, )
-# login button
-register_button = ttk.Button(signin, text="Register", command=register_clicked)
-register_button.pack(fill='x',padx=100, pady=10, expand=True, )
+            # login button
+        self.login_button = ttk.Button(self.signin, text="Login", command=self.login_clicked)
+        self.login_button.pack(fill='x',padx=100, pady=10, expand=True, )
+        # login button
+        self.register_button = ttk.Button(self.signin, text="Register",command=self.register_clicked)
+        self.register_button.pack(fill='x',padx=100, pady=10, expand=True, )
+    def login_clicked(self):
+        if LoginSys.login(self.email.get(),self.password.get()):
 
+            msg = 'You have logged in'
+            showinfo(
+                title='Information',
+                message=msg)
+        else:
+            msg = 'Incorrect email or password'
+            showinfo(
+                title='Information',
+                message=msg)
+    def register_clicked(self):
+            msg = LoginSys.add_user(self.email.get(),self.password.get())
+            showinfo(
+                title='Information',
+                message=msg)
+ 
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LoginWin(root)
+    LoginSys = LoginSystem(r'C:\Users\skyhu\Documents\Labs\test.db')
+    root.mainloop()
 
-# keep the window on
-window.mainloop()
